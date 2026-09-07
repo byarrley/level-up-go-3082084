e78015ba4ab880537b8021c58ee4df557481a817
Daniel Barrot <byarrley@gmail.com>
2026-09-07 17:04:35 -0400

    solve 01_05b

--- 01_05b/main.go
+++ 01_05b/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"flag"
 	"log"
+	"math"
 	"os"
+	"sort"
 )
 
 const path = "items.json"
@@ -20,7 +22,31 @@ type SaleItem struct {
 // matchSales adds the sales procentage of the item
 // and sorts the array accordingly.
 func matchSales(budget float64, items []SaleItem) []SaleItem {
-	panic("NOT IMPLEMENTED")
+	// Compute % off for each item
+	// Sort 'items' in descending order by % off
+	// Return the slice of items with the largest discounts that are within budget
+
+	// Solution video: was just supposed to loop over the items, display whatever was in the budget, sorted by SalesPercentage in decreasing order...not figure out what to buy ><
+
+	// Apparently 'range' returns a copy of each slice item, so use the index instead
+	for idx := range items {
+		items[idx].SalePercentage = math.Round(100 * (items[idx].OriginalPrice - items[idx].ReducedPrice) / items[idx].OriginalPrice)
+	}
+
+	// Using ">" for the "less" function so we don't have to reverse the slice
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].SalePercentage > items[j].SalePercentage
+	})
+
+	var list []SaleItem
+
+	for _, i := range items {
+		if budget > i.ReducedPrice {
+			list = append(list, i)
+			budget -= i.ReducedPrice
+		}
+	}
+	return list
 }
 
 func main() {
@@ -39,12 +65,12 @@ func printItems(items []SaleItem) {
 		log.Println("No items found.:( Try increasing your budget.")
 	}
 	for i, r := range items {
-		log.Printf("[%d]:%s is %.2f OFF! Get it now for JUST %.2f!\n", 
-		i, r.Name, r.SalePercentage, r.ReducedPrice)
+		log.Printf("[%d]:%s is %.2f OFF! Get it now for JUST %.2f!\n",
+			i, r.Name, r.SalePercentage, r.ReducedPrice)
 	}
 }
 
-// importData reads the raffle entries from file and 
+// importData reads the raffle entries from file and
 // creates the entries slice.
 func importData() []SaleItem {
 	file, err := os.ReadFile(path)
