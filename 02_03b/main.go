5f9efe67865c1323a14ac35f08f1fab34e1f35e0
Daniel Barrot <byarrley@gmail.com>
2026-09-12 12:40:35 -0400

    add task description, hints, assumptions

--- 02_03b/main.go
+++ 02_03b/main.go
@@ -1,6 +1,27 @@
 package main
 
-import "log"
+import (
+	"log"
+)
+
+// The Task: Given a defined list of resources, implement a function that simulates the concurrent allocation of resources to consumer goroutines.
+// Hint: A signal channel, `var signal chan struct{}`, is a channel whose purpose is to synchronize goroutines
+
+/* Assumptions:
+serveLunch
+- Each course takes a random amount of time to be served
+- Courses are served concurrently
+- Assume 6:1 attendee to service staff ratio
+- There are enough of each course for all consumers
+- Each course has a buffer associated with it
+takeLunch
+- Each course is received in slice order, requires a random amount of time to be taken
+- Buffet-style lunch (each consumer takes all 3 courses in order before they exit the line)
+- Line size should be limited so as not to exhaust system resources
+- Assume a single line
+- Each attendee must wait to take a course until the previous attendee has finished taking it
+- Each attendee takes exactly one of each course
+*/
 
 // the number of attendees we need to serve lunch to
 const consumerCount = 300
